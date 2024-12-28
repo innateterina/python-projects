b0e5f9106d9dd5bd53d25d986960bbde0fdaa222
innateterina <inna.charykova@gmail.com>
2024-12-28 15:15:17 +0000

    fixed errors in pong game

--- 100_days_of_code/day_22/main.py
+++ 100_days_of_code/day_22/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
+from score import Score
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,32 +13,34 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-
-def go_up():
-    ney_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), ney_y)
-
-
-def go_down():
-    ney_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), ney_y)
-
+ball = Ball()
+scoreboard = Score()
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
