ad9022e6b806212e33ef806576e7baa2b13a985b
innateterina <inna.charykova@gmail.com>
2025-02-03 22:38:11 +0000

    found and corrected errors, and changed the theme of the questions

--- 100_days_of_code/day_34/data.py
+++ 100_days_of_code/day_34/data.py
@@ -2,6 +2,7 @@ import requests
 parameters = {
     "amount": 10,
     "type": "boolean",
+    "category": 18,
 }
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
